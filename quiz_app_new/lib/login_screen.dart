bf0be6adc45b438073a7d14595937c601312bca2
Jiya-Sharma09 <jiyas4870@gmail.com>
2025-10-08 20:10:52 +0530

    giving style to login

---
+++ quiz_app_new/lib/login_screen.dart
@@ -0,0 +1,162 @@
+import 'home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'signup_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  final _jiyaKey = GlobalKey<FormState>();
+  
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _isLoading = false; 
+  String? _errorMessage = '';
+
+
+  void _login()async{
+
+    if(!_jiyaKey.currentState!.validate()){
+      // we entered this code when the validators say that the entered input bu the user is not valid.
+      return ;
+    }
+
+    // yaha hamne sab validators ko run kar liya and they return valid input
+    
+    setState(() {
+      _isLoading = true;
+      _errorMessage = '';
+    });
+
+    try{
+      await FirebaseAuth.instance.signInWithEmailAndPassword(email: _emailController.text.trim(), 
+      password: _passwordController.text.trim());
+    }on FirebaseAuthException catch(e){
+      setState(() {
+        _errorMessage = e.message;
+      });
+    }catch(e){
+      _errorMessage = 'an unexpected error ocurred !';
+    }
+
+    if(mounted){
+      setState(() {
+        _errorMessage= 'anuexpected error has occured';
+      });
+    }
+
+
+  }
+
+  @override
+  void dispose(){
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body :
+      Center
+      (child : SingleChildScrollView(
+        child: Form(
+          child: Column(
+            children: [
+
+              CircleAvatar(
+                backgroundColor: Color.fromARGB(151, 227, 97, 151),
+                child: Text('Quiziko'),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email', 
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 239, 53, 130),
+                      width: 10
+                    )
+                  ),
+                  filled: true, 
+                  fillColor: Color.fromARGB(255, 243, 115, 168)),
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return 'Please enter your password.';
+                  }
+
+                  return null;
+                } ,
+
+              ),
+
+
+            SizedBox(
+                height: 10,
+              ),
+
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Email', 
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 239, 53, 130),
+                      width: 10
+                    )
+                  ),
+                  filled: true, 
+                  fillColor: Color.fromARGB(255, 243, 115, 168)
+
+                  ),
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return 'please enter your password';
+                  }
+
+                  return null;
+
+                } ,
+
+              ),
+
+              Container(
+                height: 10,
+              ),
+
+              _isLoading ?
+              ElevatedButton(onPressed: _login, child: Text('Login'))
+              : CircularProgressIndicator(),
+
+              SizedBox(
+                height: 20,
+              ),
+
+
+              // goinggggggggg to sign up screeenn yay
+
+              TextButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupScreen()),
+                );
+              }, child: Text("Don't have an account ? then sign up"),),
+            ],
+        ),
+        ) ,      
+      ),),
+
+      backgroundColor: Color.fromARGB(251, 247, 247, 247)
+      );
+  }
+}
