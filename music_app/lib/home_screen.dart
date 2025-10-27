beae2810385de1193a21fde3a1e8fc708967336e
Jiya-Sharma09 <jiyas4870@gmail.com>
2025-10-27 15:21:13 +0530

    adding login page and few updates on homepage

--- music_app/lib/home_screen.dart
+++ music_app/lib/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class homeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      
+    );
   }
 }
